2f6a3237dbcbfce547bd868e4aa8c268ddea8aff
Dafnik <git@dafnik.me>
2025-04-29 16:20:54 +0200

    feat: change web container to port 80

--- web/src/server.ts
+++ web/src/server.ts
@@ -65,7 +65,7 @@ app.use('/**', (req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4200;
+  const port = process.env['PORT'] || 80;
   app.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`, environment);
   });
