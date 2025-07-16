aa87fd68ef8a02e1f743510d381871349da5da74
Dafnik <git@dafnik.me>
2025-07-16 22:56:53 +0200

    refactor: move HealthCheck from compose file into container

---
+++ web/HealthCheck.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+// healthcheck.js
+// Exit 0 if GET / responds 2xx, else exit 1.
+
+const url = 'http://127.0.0.1:4200';
+const timeoutMs = 10_000;
+const controller = new AbortController();
+
+// Abort the request after timeoutMs
+setTimeout(() => controller.abort(), timeoutMs);
+
+fetch(url, {signal: controller.signal})
+  .then((res) => {
+    process.exit(res.ok ? 0 : 1);
+  })
+  .catch(() => {
+    process.exit(1);
+  });
