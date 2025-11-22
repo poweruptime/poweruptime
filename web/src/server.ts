bed805bd3033a89bdc1a95bb39d3f8d0d433a115
Dafnik <git@dafnik.me>
2025-11-22 18:02:53 +0100

    fix: angular reverse proxy

--- web/src/server.ts
+++ web/src/server.ts
@@ -71,20 +71,21 @@ export async function app() {
 if (isMainModule(import.meta.url)) {
   (async () => {
     const host = process.env['HOST'] || '0.0.0.0';
-    const ports = [4200, 80]; // Add as many as you need
+    const ports = [4200, 80];
 
-    for (const port of ports) {
-      const server = await app();
-      try {
-        await server.listen({ host, port });
-        // noinspection HttpUrlsUsage
-        console.log(
-          `✅ Listening on http://${host}:${port}; backendHost: "${environment.backendHost}"`,
-        );
-      } catch (err) {
-        console.error(`❌ Failed to start server on port ${port}:`, err);
-      }
-    }
+    await Promise.all(
+      ports.map(async (port) => {
+        const server = await app();
+        try {
+          await server.listen({host, port});
+          console.log(
+            `✅ Listening on http://${host}:${port}; backendHost: "${environment.backendHost}"`,
+          );
+        } catch (err) {
+          console.error(`❌ Failed to start server on port ${port}:`, err);
+        }
+      }),
+    );
   })();
 }
 
