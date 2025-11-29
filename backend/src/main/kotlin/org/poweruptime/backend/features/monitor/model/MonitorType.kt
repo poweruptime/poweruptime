be2a763ad1c81504dce8d8edfb8917a4904f628d
Dafnik <git@dafnik.me>
2025-11-29 14:08:05 +0100

    feat: spring boot v4 update

--- backend/src/main/kotlin/org/poweruptime/backend/features/monitor/model/MonitorType.kt
+++ backend/src/main/kotlin/org/poweruptime/backend/features/monitor/model/MonitorType.kt
@@ -2,20 +2,28 @@ package org.poweruptime.backend.features.monitor.model
 
 import org.poweruptime.backend.core.models.ADatabaseEnumConvertable
 
+object MonitorTypes {
+    const val DNS = "DNS"
+    const val HTTP = "HTTP"
+    const val PING = "PING"
+    const val PUSH = "PUSH"
+    const val SSL_CERTIFICATE = "SSL_CERTIFICATE"
+}
+
 enum class MonitorType : ADatabaseEnumConvertable {
     DNS {
-        override val code = "DNS"
+        override val code = MonitorTypes.DNS
     },
     HTTP {
-        override val code = "HTTP"
+        override val code = MonitorTypes.HTTP
     },
     PING {
-        override val code = "PING"
+        override val code = MonitorTypes.PING
     },
     PUSH {
-        override val code = "PUSH"
+        override val code = MonitorTypes.PUSH
     },
     SSL_CERTIFICATE {
-        override val code = "SSL_CERTIFICATE"
+        override val code = MonitorTypes.SSL_CERTIFICATE
     },
 }
--- backend/src/main/kotlin/org/poweruptime/backend/features/notification/model/NotificationMethodData.kt
+++ backend/src/main/kotlin/org/poweruptime/backend/features/notification/model/NotificationMethodData.kt
@@ -1,44 +1,24 @@
 package org.poweruptime.backend.features.notification.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
-import com.fasterxml.jackson.databind.annotation.JsonTypeIdResolver
-import org.poweruptime.backend.core.exceptions.BadRequestException
-import org.poweruptime.backend.features.notification.core.NotificationMethodDataTypeResolver
 import org.poweruptime.backend.features.notification.core.NotificationMethodType
 import org.poweruptime.backend.features.notification.core.NotificationMethodTypes
 import org.poweruptime.backend.features.notification.notificationMethods.apprise.AppriseNotificationMethodDataRecord
 import org.poweruptime.backend.features.notification.notificationMethods.discord.DiscordNotificationMethodDataRecord
 import org.poweruptime.backend.features.notification.notificationMethods.email.EmailNotificationMethodDataRecord
 import org.poweruptime.backend.features.notification.notificationMethods.slack.SlackNotificationMethodDataRecord
-import kotlin.reflect.KClass
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "_type")
-@JsonTypeIdResolver(NotificationMethodDataTypeResolver::class)
+@JsonSubTypes(
+    JsonSubTypes.Type(value = AppriseNotificationMethodDataRecord::class, name = NotificationMethodTypes.APPRISE),
+    JsonSubTypes.Type(value = DiscordNotificationMethodDataRecord::class, name = NotificationMethodTypes.DISCORD),
+    JsonSubTypes.Type(value = EmailNotificationMethodDataRecord::class, name = NotificationMethodTypes.EMAIL),
+    JsonSubTypes.Type(value = SlackNotificationMethodDataRecord::class, name = NotificationMethodTypes.SLACK),
+)
 abstract class NotificationMethodData(
     @Suppress("PropertyName", "ConstructorParameterNaming")
     @JsonProperty("_type")
     val _type: NotificationMethodType
-) {
-    companion object {
-        private val registry = mutableMapOf<String, KClass<out NotificationMethodData>>()
-
-        private fun registerDataRecord(type: String, clazz: KClass<out NotificationMethodData>) {
-            registry[type] = clazz
-        }
-
-        fun forType(type: String): KClass<out NotificationMethodData> =
-            registry[type] ?: throw BadRequestException("Unknown notification method type: $type")
-
-        fun forClass(klass: KClass<*>?): String =
-            registry.entries.find { it.value == klass }?.key
-                ?: throw BadRequestException("Unknown notification method class: ${klass?.simpleName ?: "null"}")
-
-        init {
-            registerDataRecord(NotificationMethodTypes.APPRISE, AppriseNotificationMethodDataRecord::class)
-            registerDataRecord(NotificationMethodTypes.DISCORD, DiscordNotificationMethodDataRecord::class)
-            registerDataRecord(NotificationMethodTypes.EMAIL, EmailNotificationMethodDataRecord::class)
-            registerDataRecord(NotificationMethodTypes.SLACK, SlackNotificationMethodDataRecord::class)
-        }
-    }
-}
+)
