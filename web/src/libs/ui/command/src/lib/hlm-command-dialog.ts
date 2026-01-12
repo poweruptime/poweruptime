2fb6ebc71d266ee8f106f1e8193d2b34f8ecee16
Dafnik <git@dafnik.me>
2026-01-12 10:51:37 +0100

    chore: update spartan

---
+++ web/src/libs/ui/command/src/lib/hlm-command-dialog.ts
@@ -0,0 +1,46 @@
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  contentChild,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
+
+import {BrnCommandSearchInputToken} from '@spartan-ng/brain/command';
+import {injectExposesStateProvider} from '@spartan-ng/brain/core';
+import {classes} from '@spartan-ng/helm/utils';
+
+@Directive({
+  selector: '[hlmCommandDialog]',
+  host: {
+    'data-slot': 'command-dialog',
+  },
+})
+export class HlmCommandDialog {
+  private readonly _stateProvider = injectExposesStateProvider({host: true});
+  public readonly state = this._stateProvider.state ?? signal('closed').asReadonly();
+  private readonly _renderer = inject(Renderer2);
+  private readonly _element = inject(ElementRef);
+
+  /** Access the search field */
+  private readonly _searchInput = contentChild(BrnCommandSearchInputToken, {read: ElementRef});
+
+  constructor() {
+    classes(
+      () =>
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-top-[2%] data-[state=open]:slide-in-from-top-[2%]',
+    );
+
+    effect(() => {
+      this._renderer.setAttribute(this._element.nativeElement, 'data-state', this.state());
+
+      const searchInput = this._searchInput();
+
+      if (this.state() === 'open' && searchInput) {
+        searchInput.nativeElement.focus();
+      }
+    });
+  }
+}
---
+++ web/src/libs/ui/command/src/lib/hlm-command-separator.ts
@@ -0,0 +1,16 @@
+import {Directive} from '@angular/core';
+
+import {classes} from '@spartan-ng/helm/utils';
+
+@Directive({
+  selector: '[hlmCommandSeparator],hlm-command-separator',
+  host: {
+    'data-slot': 'command-separator',
+    role: 'separator',
+  },
+})
+export class HlmCommandSeparator {
+  constructor() {
+    classes(() => 'bg-border -mx-1 block h-px');
+  }
+}
