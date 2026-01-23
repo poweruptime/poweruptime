0f23d9872c1676dc069df35bc72e5da343b5350b
Dafnik <git@dafnik.me>
2026-01-23 09:45:35 +0100

    feat: spartan migration

--- web/src/app/components/table-loading-bar.ts
+++ web/src/app/components/table-loading-bar.ts
@@ -1,7 +1,6 @@
 import {ChangeDetectionStrategy, Component, input} from '@angular/core';
 
-
-import { HlmProgressImports} from '@spartan-ng/helm/progress';
+import {HlmProgressImports} from '@spartan-ng/helm/progress';
 
 @Component({
   template: `
