31485f0ed778808a98d80c99d467a92bd26ec71c
Dafnik <git@dafnik.me>
2026-05-05 12:34:15 +0200

    feat: relace chart library

--- web/src/app/components/monitor/ping-chart.ts
+++ web/src/app/components/monitor/ping-chart.ts
@@ -1,127 +1,54 @@
 import {DatePipe} from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  LOCALE_ID,
-  afterNextRender,
-  inject,
-  input,
-  viewChild,
-} from '@angular/core';
+import {ChangeDetectionStrategy, Component, LOCALE_ID, inject, input} from '@angular/core';
 
-import Chart, {TooltipItem} from 'chart.js/auto';
-import {ThemeService} from 'dfx-theme';
+import {AreaChartComponent, CurveType} from 'angular-chrts';
+
+import {BackendType} from '@app/api';
 
 @Component({
   template: `
-    <div class="relative h-80 w-full">
-      <canvas class="w-full" #chartView></canvas>
-    </div>
-    <div class="flex justify-between px-3">
-      @let _chart = chart();
-      <span>
-        @if (_chart.data[0]; as entry) {
-          {{ entry.name | date: 'dd.MM. HH:mm' }}
-        } @else {
-          ERROR
-        }
-      </span>
-      <span>
-        @if (_chart.data[_chart.data.length - 1]; as entry) {
-          {{ entry.name | date: 'dd.MM. HH:mm' }}
-        } @else {
-          ERROR
-        }
-      </span>
-    </div>
+    <ngx-area-chart
+      [yDomain]="[chart().smallestValue, chart().highestValue]"
+      [data]="chart().data"
+      [categories]="categories"
+      [curveType]="CurveType.MonotoneX"
+      [height]="400"
+      [xFormatter]="formatX"
+      [tooltipTitleFormatter]="formatTooltipTitle"
+      [yGridLine]="true"
+      [hideLegend]="true"
+      yLabel="Ping (ms)" />
   `,
   selector: 'pu-ping-chart',
-  imports: [DatePipe],
+  imports: [AreaChartComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PingChart {
-  private readonly locale = inject(LOCALE_ID);
-  private readonly dateFormat = new DatePipe(this.locale);
-  private readonly themeService = inject(ThemeService);
+  private readonly dateFormat = new DatePipe(inject(LOCALE_ID));
 
-  chart = input.required<{
-    data: {name: string; value: number}[];
-    smallestValue: number;
-    highestValue: number;
-  }>();
+  protected readonly CurveType = CurveType;
+  protected readonly categories = {
+    value: {
+      name: 'Ping (ms)',
+      color: '#10b981',
+    },
+  };
 
-  chartCanvas = viewChild.required<ElementRef<HTMLCanvasElement>>('chartView');
+  protected readonly formatX = (tick: number | Date) => {
+    if (typeof tick === 'number') {
+      const date = this.chart().data[tick]?.name;
+      if (!date) {
+        return '';
+      }
+      return this.dateFormat.transform(date, 'dd.MM HH:mm')!;
+    }
+    return '';
+  };
+  protected readonly formatTooltipTitle = (
+    data: BackendType['PingTimelineResponse']['data'][0],
+  ) => {
+    return this.dateFormat.transform(data.name, 'dd.MM HH:mm')!;
+  };
 
-  constructor() {
-    afterNextRender(() => {
-      const chart = this.chart();
-      new Chart(this.chartCanvas().nativeElement, {
-        type: 'line',
-        options: {
-          backgroundColor: 'rgba(16, 185, 129, 0.2)', // emerald-500 @ 20% alpha
-          borderColor: 'rgba(16, 185, 129, 1)',
-          responsive: true,
-          maintainAspectRatio: false,
-          interaction: {
-            intersect: false,
-          },
-          scales: {
-            x: {
-              ticks: {
-                display: false,
-              },
-              grid: {
-                display: false,
-              },
-            },
-            y: {
-              min: chart.smallestValue,
-              max: chart.highestValue,
-              title: {
-                display: true,
-                text: 'Ping (ms)',
-              },
-              grid: {
-                display: true,
-                drawTicks: true,
-                color:
-                  this.themeService.resolvedTheme() === 'dark'
-                    ? 'oklch(27.8% 0.033 256.848)'
-                    : 'oklch(92.8% 0.006 264.531)',
-                lineWidth: 1,
-              },
-            },
-          },
-          parsing: {
-            xAxisKey: 'name',
-            yAxisKey: 'value',
-          },
-          plugins: {
-            legend: {
-              display: false,
-            },
-            tooltip: {
-              callbacks: {
-                title(_: TooltipItem<'line'>[]): string | string[] | void {
-                  return '';
-                },
-                label: (tooltipItem: TooltipItem<'line'>): string => {
-                  return `${tooltipItem.formattedValue}ms - ${this.dateFormat.transform(tooltipItem.label, 'yyyy.MM.dd HH:mm:ss')}`;
-                },
-              },
-            },
-          },
-        },
-        data: {
-          datasets: [
-            {
-              fill: 'start',
-              data: chart.data,
-            },
-          ],
-        },
-      });
-    });
-  }
+  chart = input.required<BackendType['PingTimelineResponse']>();
 }
